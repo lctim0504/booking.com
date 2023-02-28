61fafd78e8e3217f03dd5181dd4f0fe0b38e60a7
Tim <coding8426@gmail.com>
2023-02-28 14:55:46 +0800

    navbar / header roughly finish

---
+++ bookingweb/src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import "./navbar.scss"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBed, faCar, faPlane, faTaxi, faToriiGate } from '@fortawesome/free-solid-svg-icons'
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className='navbarContainer'>
+                <div className='lineOne'>
+                    <div className='left'>
+                        <span className='logo'>Tim.BOOKING</span>
+                    </div>
+                    <div className='right'>
+                        <div className='navButtonFlag'></div>
+                        <div className='navButtonTest'>測試</div>
+                        <div className='navButton'>註冊</div>
+                        <div className='navButton'>登入</div>
+                        <div className="lineTwo">
+                        </div>
+                    </div>
+                </div>
+                <div className='lineTwo'>
+                    <div className="item active">
+                        <FontAwesomeIcon icon={faBed} /><span >住宿</span>
+                    </div>
+                    <div className="item">
+                        <FontAwesomeIcon icon={faPlane} /><span >航班</span>
+                    </div>
+                    <div className="item">
+                        <FontAwesomeIcon icon={faCar} /><span >租車</span>
+                    </div>
+                    <div className="item">
+                        <FontAwesomeIcon icon={faToriiGate} /><span >景點/活動</span>
+                    </div>
+                    <div className="item">
+                        <FontAwesomeIcon icon={faTaxi} /><span >機場計程車</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
--- bookingweb/src/pages/Home.jsx
+++ bookingweb/src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import "./home.scss"
+import Header from '../components/Header'
+import Navbar from '../components/Navbar'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div className='home'>
+            <Navbar />
+            <Header />
+        </div>
+    )
 }
 
 export default Home
