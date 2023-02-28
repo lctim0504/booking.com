cd9f27a50608eadc649cc64523fb198616602ab7
Tim <coding8426@gmail.com>
2023-02-28 19:08:30 +0800

    Add new component -Announcement

--- bookingweb/src/pages/Home.jsx
+++ bookingweb/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import "./home.scss"
 import Header from '../components/Header'
 import Navbar from '../components/Navbar'
+import Announcement from '../components/Announcement'
 
 const Home = () => {
     return (
         <div className='home'>
             <Navbar />
             <Header />
+            <Announcement />
         </div>
     )
 }
