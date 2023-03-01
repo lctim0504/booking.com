684e2de4bb4e2929f5b5fc5422d61971631b1a9a
Tim <coding8426@gmail.com>
2023-03-01 21:41:35 +0800

    add component -hotelList -searchItems

--- bookingweb/src/App.js
+++ bookingweb/src/App.js
@@ -1,10 +1,19 @@
 import "./app.scss"
 import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HotelList from "./pages/HotelList";
+import HotelInfo from "./pages/HotelInfo";
 
 function App() {
   return (
     <div className="app">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/hotelsList" element={<HotelList />} />
+          <Route path="/hotels/:id" element={<HotelInfo />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
